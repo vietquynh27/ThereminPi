76265b1a573af5610ae5afaf1b3d30094f9c3ae3
nvquynh <quynh.nguyen@haw-hamburg.de>
2017-05-06 22:46:49 +0200

    added some prints for debugging

--- src/frequency.py
+++ src/frequency.py
@@ -18,26 +18,35 @@ def prepare(GPIO_TRIGGER, GPIO_ECHO):
 #This function get the distance from the sensor      
 def get_distance(GPIO_TRIGGER, GPIO_ECHO):
     #Set output to true for 0.00001ms,
-    #this triggers the sensor to send an ultrasonic pulse (8 ultrasound burst at 40kHz) 
+    #this triggers the sensor to send an ultrasonic pulse (8 ultrasound burst at 40kHz)
+    #the ultrasonic sound will the be reflected by closed by solid object
+    #and be detected by the receiver of the sensor
     GPIO.output(GPIO_TRIGGER, True)
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
-    #The sensor 
+    print("Sent pulse")
+    #The sensor then set the echo=1 until the pulse comes back
+    #so we have to record the last timestamp before echo goes high
     try:
         while GPIO.input(GPIO_ECHO)==0:
             start = time.time()
+            print("echo is now on high, waiting for reflected pulse")
     except Exception:
         pass
+    #and record the last timestamp when echo is high
     try:
         while GPIO.input(GPIO_ECHO)==1:
             stop = time.time()
+            print("reflection detected")
     except Exception:
         pass
+    #that gives the elapsed time for the pulse to goes and comes back
     elapsed = stop - start
+    #the sound traveled double the distance, so we get
     distance = (elapsed * 34300)/2
     return int(distance)
  
-
+#This function set the parameters for the midi data
 def conf_midi():
     instrument = 79
     device_id = 2
@@ -46,8 +55,8 @@ def conf_midi():
     global midiOutput
     midiOutput = pygame.midi.Output(device_id, latency, buffer_size)  
     midiOutput.set_instrument(instrument)
-      
 
+#This function declare the range of the tone and convert the distance into the note value,
 def get_note(dist):
     minDist = 5
     maxDist = 60
@@ -65,23 +74,27 @@ def get_note(dist):
       note = maxNote - (fup/fdown)
     return int (note) 
 
+#This function defines the action of playing a tone
 def play_midi(note, b4note):
     if (note != b4note):
         midiOutput.note_off(b4note, 0)
         midiOutput.note_on(note, 127)
     
-        
+#Start module pygame midi
 pygame.init()
 pygame.midi.init()
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
+#Tell the Pi which GPIO_Pin you are using
 GPIO_TRIGGER = 23
 GPIO_ECHO = 24
 
+#Get the sensor ready
 prepare(GPIO_TRIGGER, GPIO_ECHO)
 
+#Configure the midi data
 note = 0
 conf_midi()
 
@@ -89,8 +102,9 @@ conf_midi()
 try:
     while True:
         b4note = note
+        #Create a list to record the distance values
         liste = []
-        print('starte Messung')
+        print("start measurement")
         while len(liste) < 2:
             single_dist = get_distance(GPIO_TRIGGER, GPIO_ECHO)
             print("single_dist:", single_dist)
@@ -98,14 +112,18 @@ try:
                 single_dist = dist_alt
             liste.append(single_dist)
         time.sleep(0.1)
+        #Get the median value of the list
         median_dist = statistics.median(liste)
         print("median:", median_dist)
         str(median_dist)
+        #Empty the list
         del liste [:]
+        #Get the note from the median value of the distances measured
         note = get_note(median_dist)
         print("note:", note)
         str(note)
         dist_alt = single_dist
+        #Play the newly calculated note and shut off the previous one
         play_midi(note, b4note)
         time.sleep(0.2)
         
